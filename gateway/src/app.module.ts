f02edc04276fbfb30cfbd727a8d5d3cd447ffa8c
Jessica Castro <jessica.castros@outlook.com>
2022-07-04 23:18:16 -0300

    [core] inserts config for gateway

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -1,10 +1,35 @@
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        cors: true,
+        context: ({ req }) => ({ headers: req.headers }),
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'purchases', url: 'http://localhost:4000/graphql' },
+            { name: 'classroom', url: 'http://localhost:4001/graphql' },
+          ],
+        }),
+        buildService: ({ url }) =>
+          new RemoteGraphQLDataSource({
+            url,
+            willSendRequest({ request, context }) {
+              request.http.headers.set(
+                'authorization',
+                context?.headers?.authorization,
+              );
+            },
+          }),
+      },
+    }),
+  ],
 })
 export class AppModule {}
